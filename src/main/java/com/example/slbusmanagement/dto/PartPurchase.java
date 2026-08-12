1f1ed7e3ae193a57fe521710b794ffb623fdee0d
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 13:47:11 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/PartPurchase.java
@@ -0,0 +1,45 @@
+package com.example.slbusmanagement.dto;
+
+import com.example.slbusmanagement.enumiration.RecordStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class PartPurchase {
+
+    @Id
+    private Long purchaseId;
+
+    private Long busId;
+
+    private Long maintId;
+
+    private String partName;
+
+    private Integer quantity;
+
+    private Double unitPrice;
+
+    private Double totalCost;
+
+    private String supplierName;
+
+    private String date;
+
+    private String partDescription;
+
+    private Long createdBy;
+
+
+    @Enumerated(EnumType.STRING)
+    private RecordStatus status = RecordStatus.ACTIVE;
+
+}
