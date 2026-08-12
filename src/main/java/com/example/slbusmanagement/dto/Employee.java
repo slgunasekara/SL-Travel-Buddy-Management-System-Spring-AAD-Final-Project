483767daba2ab39ea2e4516de0fb01e1cf79a9de
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 13:44:02 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/Employee.java
@@ -0,0 +1,49 @@
+package com.example.slbusmanagement.dto;
+
+import com.example.slbusmanagement.enumiration.RecordStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    private Long empId;
+
+    private String empCategory;
+
+    private String empCategory2;
+
+    private String empName;
+
+    private String address;
+
+    private String contactNo;
+
+    private String nicNo;
+
+    private String ntcNo;
+
+    private String drivingLicenceNo;
+
+    private String joinDate;
+
+    private String exitDate;
+
+    private String empStatus;
+
+    private Long createdBy;
+
+
+    @Enumerated(EnumType.STRING)
+    private RecordStatus status = RecordStatus.ACTIVE;
+
+}
