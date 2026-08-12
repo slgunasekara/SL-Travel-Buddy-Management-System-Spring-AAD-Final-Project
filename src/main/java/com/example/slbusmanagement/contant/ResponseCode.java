929b1a066bf365ce8dcc539aae7f7adb634ad31c
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 13:19:39 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/contant/ResponseCode.java
@@ -0,0 +1,6 @@
+package com.example.slbusmanagement.contant;
+
+public class ResponseCode {
+    public final static int OPERATION_SUCCESS = 0;
+    public final static int OPERATION_FAILURE = 1;
+}
---
+++ src/main/java/com/example/slbusmanagement/contant/ResponseMessage.java
@@ -0,0 +1,6 @@
+package com.example.slbusmanagement.contant;
+
+public class ResponseMessage {
+    public static String SUCCESS_MESSAGE = "Operation Successful";
+    public static String FAILED_MESSAGE = "Failed";
+}
