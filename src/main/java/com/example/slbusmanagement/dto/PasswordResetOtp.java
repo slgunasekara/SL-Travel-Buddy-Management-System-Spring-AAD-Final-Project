30048f8d28f2ef48649e2ae043b1523614b158c8
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 13:47:31 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/PasswordResetOtp.java
@@ -0,0 +1,24 @@
+package com.example.slbusmanagement.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class PasswordResetOtp {
+
+    @Id
+    private Long otpId;
+
+    private Long userId;
+    private String otpCode;
+    private String email;
+    private String createdAt;
+    private String expiresAt;
+    private boolean isUsed;
+}
