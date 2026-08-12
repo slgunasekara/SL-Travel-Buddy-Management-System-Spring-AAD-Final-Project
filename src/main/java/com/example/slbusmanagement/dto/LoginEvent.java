664800322c0c108852421dad48de142321ffed5a
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 13:45:36 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/LoginEvent.java
@@ -0,0 +1,24 @@
+package com.example.slbusmanagement.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class LoginEvent {
+
+    @Id
+    private Long loginEventId;
+
+    private Long userId;
+    private String username;
+    private String name;
+    private String role;
+    private String loginAt; // ISO instant string
+}
