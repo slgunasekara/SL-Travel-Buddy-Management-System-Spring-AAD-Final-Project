a543f4123de767aeec724d2208413f9aac74776b
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 20:09:33 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/LoginRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.slbusmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class LoginRequestDTO {
+    private String username;
+    private String password;
+}
