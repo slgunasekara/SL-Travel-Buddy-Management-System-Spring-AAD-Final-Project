226aae056e34e21f0108a594ab9a9ca4b9e92198
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 13:46:12 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/Maintenance.java
@@ -0,0 +1,41 @@
+package com.example.slbusmanagement.dto;
+
+import com.example.slbusmanagement.enumiration.RecordStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Maintenance {
+
+    @Id
+    private Long maintId;
+
+    private Long busId;
+
+    private String maintenanceType;
+
+    private String serviceDate;
+
+    private Double mileage;
+
+    private Double cost;
+
+    private String technician;
+
+    private String description;
+
+    private Long createdBy;
+
+
+    @Enumerated(EnumType.STRING)
+    private RecordStatus status = RecordStatus.ACTIVE;
+
+}
