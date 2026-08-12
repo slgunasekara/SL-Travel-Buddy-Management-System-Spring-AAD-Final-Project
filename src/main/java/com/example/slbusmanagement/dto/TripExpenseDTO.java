097f2813d9cc97eb1a2fd76484b2a2207fbbd9e1
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 20:14:33 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/TripExpenseDTO.java
@@ -0,0 +1,20 @@
+package com.example.slbusmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TripExpenseDTO {
+    private Long tripExpId;
+    private Long tripId;
+    private String date;
+    private Double fuelAmount;
+    private Double parkingAmount;
+    private Double otherAmount;
+    private String otherDescription;
+    private String notes;
+    private Long createdBy;
+}
