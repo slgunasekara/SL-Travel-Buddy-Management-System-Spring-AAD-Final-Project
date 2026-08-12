082d893475c41fb9c95b21c1074f7453390722af
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 20:07:48 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/CustomerDTO.java
@@ -0,0 +1,20 @@
+package com.example.slbusmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+    private Long customerId;
+    private String name;
+    private String contact;
+    private String nic;
+    private String email;
+    private String address;
+    private String notes;
+    private Long createdBy;
+    private String createdAt;
+}
