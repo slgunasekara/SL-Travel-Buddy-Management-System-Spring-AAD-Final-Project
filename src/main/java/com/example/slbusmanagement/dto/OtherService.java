e89bbbaf7e25fc8e794dacf3a2e58a9bc2d68f07
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 13:46:43 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/OtherService.java
@@ -0,0 +1,39 @@
+package com.example.slbusmanagement.dto;
+
+import com.example.slbusmanagement.enumiration.RecordStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OtherService {
+
+    @Id
+    private Long serviceId;
+
+    private Long tripId;
+
+    private Long busId;
+
+    private String serviceName;
+
+    private Double cost;
+
+    private String description;
+
+    private String date;
+
+    private Long createdBy;
+
+
+    @Enumerated(EnumType.STRING)
+    private RecordStatus status = RecordStatus.ACTIVE;
+
+}
