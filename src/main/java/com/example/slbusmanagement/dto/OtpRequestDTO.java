0fa317ba419c03dd331258b008a871dfaff2425b
slgunasekara <praveengunasekara7@gmail.com>
2026-08-12 20:11:27 +0530

    update

---
+++ src/main/java/com/example/slbusmanagement/dto/OtpRequestDTO.java
@@ -0,0 +1,10 @@
+package com.example.slbusmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class OtpRequestDTO {
+    private String email;
+}
