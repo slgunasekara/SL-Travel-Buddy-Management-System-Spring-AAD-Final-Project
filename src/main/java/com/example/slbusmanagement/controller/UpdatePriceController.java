d239731b53a91528e601512b4d7ae2143659a933
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 19:15:38 +0530

    add Update Price

---
+++ src/main/java/com/example/slbusmanagement/controller/UpdatePriceController.java
@@ -0,0 +1,25 @@
+package com.example.slbusmanagement.controller;
+
+import com.example.slbusmanagement.contant.CommonResponse;
+import com.example.slbusmanagement.dto.UpdatePriceDTO;
+import com.example.slbusmanagement.service.UpdatePriceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "v1/price")
+@CrossOrigin
+@RequiredArgsConstructor
+public class UpdatePriceController {
+
+    private final UpdatePriceService updatePriceService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse add(@RequestBody UpdatePriceDTO dto) {
+        UpdatePriceDTO saved = updatePriceService.add(dto);
+        return new CommonResponse(0, saved, "UpdatePrice added successfully");
+    }
+}
