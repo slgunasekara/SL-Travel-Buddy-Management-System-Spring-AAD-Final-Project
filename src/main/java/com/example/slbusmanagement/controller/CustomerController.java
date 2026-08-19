c17b2ac8aaf5320ba3b71fb51b083b07c3be57f2
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 18:29:44 +0530

    Delete Customer

--- src/main/java/com/example/slbusmanagement/controller/CustomerController.java
+++ src/main/java/com/example/slbusmanagement/controller/CustomerController.java
@@ -34,4 +34,10 @@ public class CustomerController {
         CustomerDTO updated = customerService.update(id, dto);
         return new CommonResponse(0, updated, "Customer updated successfully");
     }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse delete(@PathVariable Long id) {
+        customerService.delete(id);
+        return new CommonResponse(0, "Success", "Customer deleted successfully");
+    }
 }
