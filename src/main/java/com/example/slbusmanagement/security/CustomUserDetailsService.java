1dc38016b6d4cbcef03b18533cac026783ecc7f4
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 15:16:51 +0530

    security

---
+++ src/main/java/com/example/slbusmanagement/security/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.example.slbusmanagement.security;
+
+import com.example.slbusmanagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+}
