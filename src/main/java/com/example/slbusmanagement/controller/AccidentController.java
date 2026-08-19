883ee9c230bafb82c99dcb8717aaa6ca35bc3714
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 18:34:42 +0530

    Add Accident

---
+++ src/main/java/com/example/slbusmanagement/controller/AccidentController.java
@@ -0,0 +1,25 @@
+package com.example.slbusmanagement.controller;
+
+import com.example.slbusmanagement.contant.CommonResponse;
+import com.example.slbusmanagement.dto.AccidentDTO;
+import com.example.slbusmanagement.service.AccidentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "v1/accident")
+@CrossOrigin
+@RequiredArgsConstructor
+public class AccidentController {
+
+    private final AccidentService accidentService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse add(@RequestBody AccidentDTO dto) {
+        AccidentDTO saved = accidentService.add(dto);
+        return new CommonResponse(0, saved, "Accident added successfully");
+    }
+}
