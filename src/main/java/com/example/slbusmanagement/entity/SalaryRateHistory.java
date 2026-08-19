241024e5bba0f97d79a86ec410b602957b9a5de2
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 14:28:27 +0530

    entity rapire

---
+++ src/main/java/com/example/slbusmanagement/entity/SalaryRateHistory.java
@@ -0,0 +1,27 @@
+package com.example.slbusmanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class SalaryRateHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long historyId;
+
+    private Long empId;
+
+    private Double rate;
+
+    private String effectiveDate;
+}
