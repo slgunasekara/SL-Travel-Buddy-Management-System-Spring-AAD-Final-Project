c05407859e895aac8edd0ee31aedfd27a96f8c89
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 15:20:55 +0530

    Service update

--- src/main/java/com/example/slbusmanagement/service/AccidentService.java
+++ src/main/java/com/example/slbusmanagement/service/AccidentService.java
@@ -5,8 +5,10 @@ import com.example.slbusmanagement.dto.AccidentDTO;
 import java.util.List;
 
 public interface AccidentService {
+
     List<AccidentDTO> getAll();
     AccidentDTO add(AccidentDTO dto);
     AccidentDTO update(Long id, AccidentDTO dto);
     void delete(Long id);
+
 }
