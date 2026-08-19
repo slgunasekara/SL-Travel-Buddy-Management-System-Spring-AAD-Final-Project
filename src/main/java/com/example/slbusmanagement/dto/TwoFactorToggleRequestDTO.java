45805187a7c812f2107cf6e0efa97e0498371171
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 14:56:16 +0530

    entity rapire

---
+++ src/main/java/com/example/slbusmanagement/dto/TwoFactorToggleRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.slbusmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TwoFactorToggleRequestDTO {
+    private boolean enabled;
+}
