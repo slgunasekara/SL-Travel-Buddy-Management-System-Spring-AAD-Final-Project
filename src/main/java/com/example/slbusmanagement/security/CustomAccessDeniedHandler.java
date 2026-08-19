9cde6e12386f9318db233d4e95d55e42eb2f2c4a
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 15:14:49 +0530

    security

---
+++ src/main/java/com/example/slbusmanagement/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package com.example.slbusmanagement.security;
+
+import com.example.slbusmanagement.contant.CommonResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+
+}
