71a923a31cda40b43e9b4af49b089312aaaaccaf
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 15:18:12 +0530

    security

--- src/main/java/com/example/slbusmanagement/security/CustomUserDetailsService.java
+++ src/main/java/com/example/slbusmanagement/security/CustomUserDetailsService.java
@@ -14,5 +14,18 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.example.slbusmanagement.entity.User user = userRepository.findAll().stream()
+                .filter(u -> u.getStatus() == com.example.slbusmanagement.enumiration.RecordStatus.ACTIVE)
+                .filter(u -> u.getUsername().equalsIgnoreCase(username))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: " + username));
 
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
 }
