c2677966450552ad49fb6875d20eea952bfee3c1
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 16:16:43 +0530

    add user service

--- src/main/java/com/example/slbusmanagement/controller/UserController.java
+++ src/main/java/com/example/slbusmanagement/controller/UserController.java
@@ -18,27 +18,10 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
-    public CommonResponse getAll() {
-        List<UserDTO> items = userService.getAll();
-        return new CommonResponse(0, items, "Get all users");
-    }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CommonResponse add(@RequestBody UserDTO dto) {
         UserDTO saved = userService.add(dto);
         return new CommonResponse(0, saved, "User added successfully");
-    }
-
-    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public CommonResponse update(@PathVariable Long id, @RequestBody UserDTO dto) {
-        UserDTO updated = userService.update(id, dto);
-        return new CommonResponse(0, updated, "User updated successfully");
-    }
-
-    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public CommonResponse delete(@PathVariable Long id) {
-        userService.delete(id);
-        return new CommonResponse(0, "Success", "User deleted successfully");
     }
 }
