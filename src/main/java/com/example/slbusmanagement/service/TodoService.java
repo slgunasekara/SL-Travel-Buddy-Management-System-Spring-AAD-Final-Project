27eb0d1ad71a7dc17f30a1beeca1a4b496761db3
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 15:35:29 +0530

    Service update

--- src/main/java/com/example/slbusmanagement/service/TodoService.java
+++ src/main/java/com/example/slbusmanagement/service/TodoService.java
@@ -1,8 +1,12 @@
 package com.example.slbusmanagement.service;
 
-
+import com.example.slbusmanagement.dto.TodoDTO;
 
 import java.util.List;
 
 public interface TodoService {
+    List<TodoDTO> getAll();
+    TodoDTO add(TodoDTO dto);
+    TodoDTO update(Long id, TodoDTO dto);
+    void delete(Long id);
 }
