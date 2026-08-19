473548760419817924c77395029635d7bbc7a99b
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 16:52:53 +0530

    Employee service

---
+++ src/main/java/com/example/slbusmanagement/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.slbusmanagement.service.impl;
+
+import com.example.slbusmanagement.dto.EmployeeDTO;
+import com.example.slbusmanagement.entity.Employee;
+import com.example.slbusmanagement.entity.SalaryRateHistory;
+import com.example.slbusmanagement.enumiration.RecordStatus;
+import com.example.slbusmanagement.exception.CustomeException;
+import com.example.slbusmanagement.repository.EmployeeRepository;
+import com.example.slbusmanagement.repository.SalaryRateHistoryRepository;
+import com.example.slbusmanagement.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final SalaryRateHistoryRepository salaryRateHistoryRepository;
+
+    private EmployeeDTO toDto(Employee entity) {
+        EmployeeDTO dto = new EmployeeDTO();
+        BeanUtils.copyProperties(entity, dto);
+        return dto;
+    }
+
+    @Override
+    public List<EmployeeDTO> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public EmployeeDTO add(EmployeeDTO dto) {
+        return null;
+    }
+
+    @Override
+    public EmployeeDTO update(Long id, EmployeeDTO dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
