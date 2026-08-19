4bad349906dcaa9ca68f5f183e44a733b3c96965
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 17:14:44 +0530

    Add Employee Salary

---
+++ src/main/java/com/example/slbusmanagement/controller/EmployeeSalaryController.java
@@ -0,0 +1,25 @@
+package com.example.slbusmanagement.controller;
+
+import com.example.slbusmanagement.contant.CommonResponse;
+import com.example.slbusmanagement.dto.EmployeeSalaryDTO;
+import com.example.slbusmanagement.service.EmployeeSalaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "v1/salary")
+@CrossOrigin
+@RequiredArgsConstructor
+public class EmployeeSalaryController {
+
+    private final EmployeeSalaryService employeeSalaryService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse add(@RequestBody EmployeeSalaryDTO dto) {
+        EmployeeSalaryDTO saved = employeeSalaryService.add(dto);
+        return new CommonResponse(0, saved, "EmployeeSalary added successfully");
+    }
+}
