9c2a50d47936077aac051148ea5ad71f1b5798c4
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 18:36:03 +0530

    Gett All Accident

--- src/main/java/com/example/slbusmanagement/controller/AccidentController.java
+++ src/main/java/com/example/slbusmanagement/controller/AccidentController.java
@@ -17,6 +17,12 @@ public class AccidentController {
 
     private final AccidentService accidentService;
 
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse getAll() {
+        List<AccidentDTO> items = accidentService.getAll();
+        return new CommonResponse(0, items, "Get all Accident");
+    }
+
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CommonResponse add(@RequestBody AccidentDTO dto) {
         AccidentDTO saved = accidentService.add(dto);
