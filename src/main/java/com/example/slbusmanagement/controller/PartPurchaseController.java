6f00989d6ac835e8cdd6713e489d27d1899364e8
slgunasekara <praveengunasekara7@gmail.com>
2026-08-19 21:03:16 +0530

    Add PartPurchase Service

---
+++ src/main/java/com/example/slbusmanagement/controller/PartPurchaseController.java
@@ -0,0 +1,27 @@
+package com.example.slbusmanagement.controller;
+
+import com.example.slbusmanagement.contant.CommonResponse;
+import com.example.slbusmanagement.dto.PartPurchaseDTO;
+import com.example.slbusmanagement.service.PartPurchaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "v1/part-purchase")
+@CrossOrigin
+@RequiredArgsConstructor
+public class PartPurchaseController {
+
+    private final PartPurchaseService partPurchaseService;
+
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse add(@RequestBody PartPurchaseDTO dto) {
+        PartPurchaseDTO saved = partPurchaseService.add(dto);
+        return new CommonResponse(0, saved, "PartPurchase added successfully");
+    }
+
+}
---
+++ src/main/java/com/example/slbusmanagement/service/impl/PartPurchaseServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.slbusmanagement.service.impl;
+
+import com.example.slbusmanagement.dto.PartPurchaseDTO;
+import com.example.slbusmanagement.entity.PartPurchase;
+import com.example.slbusmanagement.enumiration.RecordStatus;
+import com.example.slbusmanagement.exception.CustomeException;
+import com.example.slbusmanagement.repository.PartPurchaseRepository;
+import com.example.slbusmanagement.service.PartPurchaseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PartPurchaseServiceImpl implements PartPurchaseService {
+
+    private final PartPurchaseRepository partPurchaseRepository;
+
+    private PartPurchaseDTO toDto(PartPurchase entity) {
+        PartPurchaseDTO dto = new PartPurchaseDTO();
+        BeanUtils.copyProperties(entity, dto);
+        return dto;
+    }
+
+
+    @Override
+    public List<PartPurchaseDTO> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public PartPurchaseDTO add(PartPurchaseDTO dto) {
+        log.info("Save PartPurchase Method Executed....");
+        PartPurchase entity = new PartPurchase();
+        BeanUtils.copyProperties(dto, entity);
+        entity.setStatus(RecordStatus.ACTIVE);
+        partPurchaseRepository.save(entity);
+        log.info("PartPurchase Saved Successfully....");
+        return toDto(entity);
+    }
+
+    @Override
+    public PartPurchaseDTO update(Long id, PartPurchaseDTO dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+}
